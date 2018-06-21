d4b51b6553c7f82b8715d65e97ff06ff026ee149
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-21 23:01:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 dna = "ATTGC"
 def DNA_strand(dna)
     #your code here
-    out = []
+    out = ""
     dna.each_char do|char|
-        out << T if char == A
-        out << A if char == T
-        out << C if char == G
-        out << G if char == C
+        out += "T" if char == "A"
+        out += "A" if char == "T"
+        out += "C" if char == "G"
+        out += "G" if char == "C"
     end
     out
 end
