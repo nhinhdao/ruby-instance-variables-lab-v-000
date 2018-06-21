9ebe8f8b21459487b7f321d8458287a61d85c9b0
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-21 22:44:17 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+dna = "ATTGC"
+def DNA_strand(dna)
+    #your code here
+    out = []
+    dna.each_char do|char|
+        out << T if char == A
+        out << A if char == T
+        out << C if char == G
+        out << G if char == C
+    end
+    out
+end
+
+puts DNA_strand(dna)
